8f4319dcd1c610961da2d6d5daa75514c0fe0a38
nmrhard <nmrhard@gmail.com>
2020-10-04 00:17:17 +0300

    Homework 6

--- main.js
+++ main.js
@@ -41,6 +41,7 @@ btn.addEventListener('click', function () {
     enemy.changeHP(random(20));
 })
 
+
 superBtn.addEventListener('click', function () {
     character.changeHP(random(20, true));
     enemy.changeHP(random(20, true));
@@ -100,18 +101,18 @@ function random(num, isSuper) {
 function generateLog(firstPerson, secondPerson, kick) {
     const { name: firstPersonName, demageHP, defaultHP } = firstPerson;
     const { name: secondPersonName } = secondPerson;
-
+    
     const logs = [
-        `${firstPersonName} вспомнил что-то важное, но неожиданно ${secondPersonName}, не помня себя от испуга, ударил в предплечье врага. -${kick}, [${demageHP}/${defaultHP}]`,
-        `${firstPersonName} поперхнулся, и за это ${secondPersonName} с испугу приложил прямой удар коленом в лоб врага. -${kick}, [${demageHP}/${defaultHP}]`,
-        `${firstPersonName} забылся, но в это время наглый ${secondPersonName}, приняв волевое решение, неслышно подойдя сзади, ударил. -${kick}, [${demageHP}/${defaultHP}]`,
-        `${firstPersonName} пришел в себя, но неожиданно ${secondPersonName} случайно нанес мощнейший удар -${kick}, [${demageHP}/${defaultHP}].`,
-        `${firstPersonName} поперхнулся, но в это время ${secondPersonName} нехотя раздробил кулаком \<вырезанно цензурой\> противника. -${kick}, [${demageHP}/${defaultHP}]`,
-        `${firstPersonName} удивился, а ${secondPersonName} пошатнувшись влепил подлый удар. -${kick}, [${demageHP}/${defaultHP}]`,
-        `${firstPersonName} высморкался, но неожиданно ${secondPersonName} провел дробящий удар. -${kick}, [${demageHP}/${defaultHP}]`,
-        `${firstPersonName} пошатнулся, и внезапно наглый ${secondPersonName} беспричинно ударил в ногу противника -${kick}, [${demageHP}/${defaultHP}]`,
-        `${firstPersonName} расстроился, как вдруг, неожиданно ${secondPersonName} случайно влепил стопой в живот соперника. -${kick}, [${demageHP}/${defaultHP}]`,
-        `${firstPersonName} пытался что-то сказать, но вдруг, неожиданно ${secondPersonName} со скуки, разбил бровь сопернику. -${kick}, [${demageHP}/${defaultHP}]`
+        `${firstPersonName} вспомнил что-то важное, но неожиданно ${secondPersonName}, не помня себя от испуга, ударил в предплечье врага. \n -${kick}, [${demageHP}/${defaultHP}]`,
+        `${firstPersonName} поперхнулся, и за это ${secondPersonName} с испугу приложил прямой удар коленом в лоб врага. \n -${kick}, [${demageHP}/${defaultHP}]`,
+        `${firstPersonName} забылся, но в это время наглый ${secondPersonName}, приняв волевое решение, неслышно подойдя сзади, ударил. \n -${kick}, [${demageHP}/${defaultHP}]`,
+        `${firstPersonName} пришел в себя, но неожиданно ${secondPersonName} случайно нанес мощнейший удар \n -${kick}, [${demageHP}/${defaultHP}].`,
+        `${firstPersonName} поперхнулся, но в это время ${secondPersonName} нехотя раздробил кулаком \<вырезанно цензурой\> противника. \n -${kick}, [${demageHP}/${defaultHP}]`,
+        `${firstPersonName} удивился, а ${secondPersonName} пошатнувшись влепил подлый удар. \n -${kick}, [${demageHP}/${defaultHP}]`,
+        `${firstPersonName} высморкался, но неожиданно ${secondPersonName} провел дробящий удар. \n -${kick}, [${demageHP}/${defaultHP}]`,
+        `${firstPersonName} пошатнулся, и внезапно наглый ${secondPersonName} беспричинно ударил в ногу противника \n -${kick}, [${demageHP}/${defaultHP}]`,
+        `${firstPersonName} расстроился, как вдруг, неожиданно ${secondPersonName} случайно влепил стопой в живот соперника. \n -${kick}, [${demageHP}/${defaultHP}]`,
+        `${firstPersonName} пытался что-то сказать, но вдруг, неожиданно ${secondPersonName} со скуки, разбил бровь сопернику. \n -${kick}, [${demageHP}/${defaultHP}]`
     ];
 
     return logs[random(logs.length - 1)];
@@ -123,5 +124,30 @@ function renderLogs (log) {
 
     elementLogsList.insertBefore(elementLogText, elementLogsList.children[0]);
 }
+
+const maxCountKicksBtnKick = 8;
+const maxCountKicksBtnSuperKick = 2;
+
+const textKickButton = btn.innerText;
+const textSuperKickButton = superBtn.innerText;
+
+btn.innerText += ` (${maxCountKicksBtnKick})`;
+superBtn.innerText += ` (${maxCountKicksBtnSuperKick})`;
+
+function checkKicks(maxCountKicks, textButton) {
+    return function () {
+        maxCountKicks -= 1;
+        this.innerText = `${textButton} (${maxCountKicks})`;
+        if (maxCountKicks === 0) {
+            this.disabled = true;
+        }
+    }
+}
+
+const countKickButtonClick = checkKicks(maxCountKicksBtnKick, textKickButton);
+const countSuperKickButtonClick = checkKicks(maxCountKicksBtnSuperKick, textSuperKickButton);
+
+btn.addEventListener('click', countKickButtonClick);
+superBtn.addEventListener('click', countSuperKickButtonClick);
 
 init();
